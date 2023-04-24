0a68a1835687b929a2e1e4d08767d9cc870e6e45
taihuynhz <129383417+taihuynhz@users.noreply.github.com>
2023-04-24 10:29:21 +0700

    Read CSV File

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadCSVFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader reader = new StreamReader(@"C:\test.csv"))
+            {
+                List<string> list = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    string[] values = line.Split();
+
+                    list.Add(values[0]);
+                }
+
+                foreach (string element in list)
+                    Console.WriteLine(element);
+            }
+        }
+    }
+}
